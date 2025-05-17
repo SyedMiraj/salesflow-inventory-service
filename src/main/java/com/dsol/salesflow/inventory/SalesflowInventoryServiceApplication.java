62af7982b2f20790535044b7f61a7acaed436025
Shahriar Miraj <smiraj2507@gmail.com>
2025-05-17 07:37:57 +0600

    first commit

---
+++ src/main/java/com/dsol/salesflow/inventory/SalesflowInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dsol.salesflow.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesflowInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesflowInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsol/salesflow/inventory/SalesflowInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsol.salesflow.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesflowInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
